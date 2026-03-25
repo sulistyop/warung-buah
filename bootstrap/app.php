8f91b8f2c1d54778170c2de28a07f5c99f081845
sulistyop <sulistyopradana59@gmail.com>
2026-03-25 10:29:05 +0700

    remove stateful API middleware from application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
         $middleware->appendToGroup('api', ApiLogger::class);
         $middleware->alias(['permission' => CheckPermission::class]);
     })
